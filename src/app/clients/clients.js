bab082f00e5ed36f5fc08b5730452941b95ec054
Malte Münchert <malte.muenchert@gmx.net>
2015-05-11 23:48:49 +0200

    Add localStorage support to keep list of clients

--- src/app/clients/clients.js
+++ src/app/clients/clients.js
@@ -17,17 +17,27 @@ angular.module( 'billify.clients', [
   });
 })
 
-.controller( 'ClientsCtrl', function ( $scope ) {
-  $scope.clients = [];
+.controller( 'ClientsCtrl', function ($scope) {
+
+  var saveClients = function() {
+    localStorage.setItem('clients', JSON.stringify($scope.clients));
+    console.log('saving', $scope.clients);
+  };
+
+  $scope.clients = JSON.parse(localStorage.getItem('clients', $scope.clients, JSON.stringify([]))) || [];
+
   $scope.newClient = { name:'', address:'' };
 
   $scope.addClient = function() {
     $scope.clients.push(_.clone($scope.newClient));
+    saveClients();
   };
 
   $scope.removeClient = function(index) {
     delete $scope.clients[index];
     $scope.clients = _.compact($scope.clients);
+    saveClients();
   };
+
 })
 ;
