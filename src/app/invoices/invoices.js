ad35f45f67170edbc33f364d3a9d505068ba9360
Malte Münchert <malte.muenchert@gmx.net>
2015-05-16 23:33:54 +0200

    Add invoices controller

---
+++ src/app/invoices/invoices.js
@@ -0,0 +1,63 @@
+angular.module( 'billify.invoices', [
+  'ui.router',
+  'placeholders',
+  'ui.bootstrap'
+])
+
+.config(function config( $stateProvider ) {
+  $stateProvider.state( 'invoices', {
+    url: '/invoices',
+    views: {
+      "main": {
+        controller: 'InvoicesCtrl',
+        templateUrl: 'invoices/index.tpl.html'
+      }
+    },
+    data: { pageTitle: 'Invoice' }});
+
+  $stateProvider.state( 'invoices', {
+    url: '/invoice/new',
+    views: {
+      "main": {
+        controller: 'NewInvoiceCtrl',
+        templateUrl: 'invoices/new.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'New invoice' }
+  });
+})
+
+.controller( 'InvoicesCtrl', function($scope) {
+  $scope.invoices = JSON.parse(localStorage.getValue('invoices') || "[]");
+})
+.controller( 'NewInvoiceCtrl', function InvoicesCtrl( $scope ) {
+  // This is simple a demo for UI Boostrap.
+  $scope.lineItems = [
+    { title: 'AngularJS Entwicklung',
+      description: '(Stundenweise)',
+      units: 140,
+      pricePerUnit: 62.50 },
+    { title: 'Bespassung',
+      description: '(Stundenweise)',
+      units: 2,
+      pricePerUnit: 25 }
+  ];
+
+  $scope.sumNetto = function() {
+    return _.reduce($scope.lineItems, function(akk, item) {
+      return akk + 1 * (item.units * item.pricePerUnit);
+    }, 0);
+  };
+
+  $scope.addItem = function() {
+    $scope.lineItems.push({
+      title: 'New item',
+      description: '...',
+      units: 1,
+      pricePerUnit: 1,
+      nn: true
+    });
+  };
+})
+
+;
